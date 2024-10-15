af18df46c94064da78cc473149b6f1ad91d0eea2
zekiyat61 <zekiyatatsh2009@gmail.com>
2024-10-15 10:20:59 +0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,10 @@
+const colours =['red','green','blue'];
+const[firistColor,secondColour,thirdColur]=colours;
+console.log(colours)
+
+const person={ name:'Alice',age:25,city:'wonderland'};
+const{name,age,city} =person;                           
+console.log(person)
+const settings={ theme:'dark',lanuage:'en'};
+const{theme,lanuage,mode='light'}=settings;
+console.log(settings,mode)
